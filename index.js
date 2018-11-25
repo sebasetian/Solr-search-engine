645f5df522988b71573594cdfa6fc4317b332036
shih-yao <sebasetian123@gmail.com>
2018-11-25 12:46:59 -0800

    finish spell check

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 4000;
 const solrNode = require("solr-node");
+const spell = require('spell');
+const dict = spell();
+const fs = require('fs');
 const client = new solrNode({
 	host: '127.0.0.1',
 	port: '8983',
 	core: 'myexample',
 	protocol: 'http'
 });
-
 app.use(express.static(__dirname));
 app.set('port', port);
-app.get('/', (req, res) =>
-	res.sendFile('index.html')
-);
+app.get('/', (req, res) => {
+	res.sendFile('index.html');
+});
+app.get('/load-dict',(req,res) => {
+	console.log('loading');
+	let text = fs.readFileSync("big.txt", "utf-8");
+	console.log('training');
+	dict.load(text);
+	res.send("");
+})
+app.get('/correct/', (req,res) => {
+	let correctedwords = dict.suggest(req.query.word);
+	if (!correctedwords || !correctedwords[0].hasOwnProperty("word")) {
+		 res.send("");
+	}
+	else res.send(correctedwords[0].word);
+})
 app.get('/search/', (req, res) => {
 	client.search("q=" + req.query.text + "&fl=" + req.query.fl+ "&sort=" + req.query.sort)
 		.then(data => {
--- script.js
+++ script.js
@@ -1,41 +1,106 @@
 let dict = {};
+let isTrainFinished = false;
 $(document).ready(function () {
 	$.ajax({
 		type: "GET",
 		url: "URLtoHTML_mercury.csv",
 		dataType: "text",
-		success: function (data) { processData(data); }
+		success: function (data) { 
+			processData(data);
+		}, error: function (xhr, status, error) {
+			console.log(error);
+		}
+	});
+	$.get('/load-dict/',function () {
+		alert("Everything is set up. You can search now.");
+		
 	});
 	$("button").click(function () {
-		if (Object.keys(dict).length != 0) {
-			$.get('/search?fl=id,title,og_description,og_url&text=' + $('#query').val() + '&sort=' + $('#select').val() + ' desc',data => {
-				$('#result').removeAttr('hidden');
-				$('#result').empty();
-				let items = data.response.docs;
-				for (let i = 0; i < 10;i++) {
-					let content = "<h5>Result"+ (i+1) +": </h5><br><table>"
+		onSubmit(true, null);
+	});
+});
+function onSubmit(isCorrectNeed,text) {
+	if (Object.keys(dict).length != 0) {
+		let words = $('#query').val().split(' ');
+		let correctArr = [];
+		let isCorrected = false;
+		let correctedStr = "";
+		let len = 0;
+		if (isCorrectNeed) {
+			let space = "";
+			for (let i = 0; i < words.length;i++) {
+				let word = words[i];
+				$.get('/correct?word=' + word,(corrected) => {
+					if (corrected.length != 0 && corrected !== word) {
+						isCorrected = true;
+					} else {
+						corrected = word;
+					}
+					correctedStr += space + corrected;
+					correctArr[word] = corrected;
+					len++;
+					space = " ";
+					if (len == words.length) {
+						doQuery(correctedStr, isCorrected, correctArr, words);
+					}
+				});
+			}
+		} else {
+			correctedStr = text;
+			doQuery(correctedStr, isCorrected, correctArr, words);
+		}
+}
+function doQuery(correctedStr, isCorrected, correctArr, words) {
+		$.get('/search?fl=id,title,og_description,og_url&text=' + correctedStr + '&sort=' + $('#select').val() + ' desc', data => {
+			$('#result').removeAttr('hidden');
+			$('#result').empty();
+			let items = data.response.docs;
+			if (isCorrected) {
+				let showText = "<p style='font-size:120%'>Showing results for <a href='#' id='correctText'>";
+				words.forEach(word => {
+					if (correctArr[word] != word) {
+						showText += "<em>" + correctArr[word] + "</em> ";
+					} else {
+						showText += word + " ";
+					}
+				});
+				showText += "</a></p><br>Search instead for <a href='#' id='originText'>" + $('#query').val() + "</a>";
+				$('#result').append(showText);
+				$('#correctText').click(function () {
+					$('#query').val(correctedStr);
+					onSubmit(false, correctedStr);
+				});
+				$('#originText').click(function () {
+					onSubmit(false, $('#query').val());
+				});
+			}
+			if (items.length != 0) {
+				for (let i = 0; i < Math.min(items.length,10); i++) {
+					let content = "<h5>Result" + (i + 1) + ": </h5><br><table>"
 					let item = items[i];
 					let keys = Object.keys(item);
 					let values = Object.values(item);
-					let url = values[values.length-1];
-					if (keys[keys.length-1] !== 'og_url') {
+					let url = values[values.length - 1];
+					if (keys[keys.length - 1] !== 'og_url') {
 						url = dict[values[0]];
 					}
 					content += '<tr style="width:100vw"><td style="width:20vw">Key</td><td>Value<td></tr>';
 					for (let j = 0; j < keys.length; j++) {
-						if (keys[j]=="title" || keys[j]=='og_url') {
+						if (keys[j] == "title" || keys[j] == 'og_url') {
 							content += '<tr style="width:100vw"><td style="width:20vw">' + keys[j] + '</td><td><a href=\"' + url + '\">' + values[j] + '</a><td></tr>';
 						} else {
-							content += '<tr style="width:100vw"><td style="width:20vw">' + keys[j] + '</td><td>' + values[j]  +'<td></tr>';
+							content += '<tr style="width:100vw"><td style="width:20vw">' + keys[j] + '</td><td>' + values[j] + '<td></tr>';
 						}
 					}
-					content += "</table><br>"; 
+					content += "</table><br>";
 					$('#result').append(content);
 				}
-			});
-		}
-	});
-});
+			} else {
+				$('#result').append("<h5>No Record</h5>");
+			}
+		});
+	} 
+}
 function processData(allText) {
 	let allTextLines = allText.split(/\n/);
 	for (let i = 1; i < allTextLines.length; i++) {
